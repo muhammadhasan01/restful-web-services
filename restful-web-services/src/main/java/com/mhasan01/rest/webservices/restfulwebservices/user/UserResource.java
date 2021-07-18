f901d271fb20d82df1c2ca3a0890d9e5cdeaed3b
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-18 21:48:23 +0700

    feat: add post request

--- restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,9 +1,7 @@
 package com.mhasan01.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -12,13 +10,18 @@ public class UserResource {
     @Autowired
     UserDaoService service;
 
-    @GetMapping(path = "/users")
+    @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
-    @GetMapping(path = "/users/{id}")
+    @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
     }
 }
