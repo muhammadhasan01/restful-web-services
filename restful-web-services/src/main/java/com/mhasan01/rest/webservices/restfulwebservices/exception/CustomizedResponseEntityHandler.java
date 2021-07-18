bfd8e52c24344f19a3493747592e1bb03cdffd5f
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-18 22:30:03 +0700

    chore: handle specific for user not found

--- restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityHandler.java
+++ restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityHandler.java
@@ -1,5 +1,6 @@
 package com.mhasan01.rest.webservices.restfulwebservices.exception;
 
+import com.mhasan01.rest.webservices.restfulwebservices.user.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,5 +19,12 @@ public class CustomizedResponseEntityHandler extends ResponseEntityExceptionHand
         ExceptionResponse exceptionResponse =
                 new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 }
