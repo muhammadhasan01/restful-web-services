1a51842b042ef931702ce2c30cd068e4fae1a832
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-18 21:24:09 +0700

    feat: add UserResource

---
+++ restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,24 @@
+package com.mhasan01.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+    @Autowired
+    UserDaoService service;
+
+    @GetMapping(path = "/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}
