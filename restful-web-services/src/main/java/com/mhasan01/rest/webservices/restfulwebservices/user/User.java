d3d1b369bdc960e7e48731b26e941e5faeaf6df2
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-19 21:42:14 +0700

    chore: add some validation for creating user

--- restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/user/User.java
@@ -3,12 +3,16 @@ package com.mhasan01.rest.webservices.restfulwebservices.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
+    @Size(min = 2)
     private String name;
+    @Past
     private Date birthDate;
 }
