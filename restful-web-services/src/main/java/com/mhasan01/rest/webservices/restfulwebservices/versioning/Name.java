efe817ed5500e9e8a8546c849966e67c9a3a39d2
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-28 16:39:29 +0700

    feat: add basic versioning API

---
+++ restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/versioning/Name.java
@@ -0,0 +1,11 @@
+package com.mhasan01.rest.webservices.restfulwebservices.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Name {
+    private String firstName;
+    private String lastName;
+}
