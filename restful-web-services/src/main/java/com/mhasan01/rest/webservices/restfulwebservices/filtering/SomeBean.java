0b447efa72258744efbe7999336b8ade69dd0a6f
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-07-28 16:32:29 +0700

    feat: add dynamic filtering

--- restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,14 +1,14 @@
 package com.mhasan01.rest.webservices.restfulwebservices.filtering;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
     private String field1;
     private String field2;
-    @JsonIgnore
     private String field3;
 }
