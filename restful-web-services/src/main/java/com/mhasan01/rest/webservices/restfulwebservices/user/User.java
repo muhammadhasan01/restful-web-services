9975ef73e9cc124cfe548d41ea977f9e12443350
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2021-08-03 14:31:33 +0700

    feat: create user entity and some test data

--- restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/mhasan01/rest/webservices/restfulwebservices/user/User.java
@@ -2,14 +2,22 @@ package com.mhasan01.rest.webservices.restfulwebservices.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have at least two characters")
     private String name;
